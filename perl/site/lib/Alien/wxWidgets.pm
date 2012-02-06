7c4ca130af059f7a09b446530d7dfaf5174ce0c0
Gabor Szabo <szabgab@gmail.com>
2012-02-06 16:04:14 +0200

    Alien::wxWidgets               0.53    0.54

--- perl/site/lib/Alien/wxWidgets.pm
+++ perl/site/lib/Alien/wxWidgets.pm
@@ -43,7 +43,7 @@ use Module::Pluggable sub_name      => '_list',
                       instantiate   => 'config';
 
 our $AUTOLOAD;
-our $VERSION = '0.53';
+our $VERSION = '0.54';
 our %VALUES;
 our $dont_remap;
 
